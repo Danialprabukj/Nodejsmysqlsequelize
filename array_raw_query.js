b4ffd193b9f54c6ea076ff5ce0e38bab9872632e
danialprabukj <danialprabu03@gmail.com>
2023-08-21 22:55:06 +0530

    first commit

---
+++ array_raw_query.js
@@ -0,0 +1,29 @@
+const {Sequelize, DataTypes} = require("sequelize");
+
+const sequelize = new Sequelize(
+   'nodejssequelize',
+   'root',
+   '',
+    {
+      host: 'localhost',
+      dialect: 'mysql'
+    }
+   );
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
+
+sequelize.query(
+    'SELECT * FROM students WHERE student_id = ?',
+    {
+      replacements: ['4dcb559e-e48d-4ee5-a2d4-81f2219fd275'],
+      type: sequelize.QueryTypes.SELECT
+    }
+).then(result => {
+    console.log(result);
+}).catch((error) => {
+    console.error('Failed to insert data : ', error);
+});
